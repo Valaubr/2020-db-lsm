8583e6f314f72129067407021381524ac359e900
unknown <stalker039906@gmail.com>
2020-05-05 10:03:43 +0300

    Fix no source inside.

--- src/main/java/ru/mail/polis/valaubr/LSMDAO.java
+++ src/main/java/ru/mail/polis/valaubr/LSMDAO.java
@@ -1,4 +1,122 @@
 package ru.mail.polis.valaubr;
 
-public class LSNDAO {
+import com.google.common.collect.Iterators;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jetbrains.annotations.NotNull;
+import ru.mail.polis.DAO;
+import ru.mail.polis.Iters;
+import ru.mail.polis.Record;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+public class LsmDAO implements DAO {
+
+    private static final Logger logger = Logger.getLogger(LsmDAO.class.getName());
+
+    private static ByteBuffer EMPTY_BUFFER = ByteBuffer.allocate(0);
+
+    private static final String FILE_POSTFIX = ".dat";
+    private static final String TEMP_FILE_POSTFIX = ".tmp";
+
+    @NonNull
+    private final File storage;
+    private final long flushThreshold;
+
+    private final MemTable memtable;
+    private final NavigableMap<Integer, Table> ssTables;
+
+    private int generation;
+
+    public LsmDAO(
+            @NotNull final File storage,
+            final long flushThreshold) throws IOException {
+        this.storage = storage;
+        this.flushThreshold = flushThreshold;
+        this.memtable = new MemTable();
+        this.ssTables = new TreeMap<>();
+        try (Stream<Path> files = Files.list(storage.toPath())) {
+            files.filter(file -> !file.toFile().isDirectory() && file.toString().endsWith(FILE_POSTFIX))
+                    .forEach(file -> {
+                        final String fileName = file.getFileName().toString();
+                        try {
+                            final int gen = Integer.parseInt(fileName.substring(0, fileName.indexOf(FILE_POSTFIX)));
+                            generation = Math.max(gen, generation);
+                            ssTables.put(gen, new SSTable(file.toFile()));
+                        } catch (IOException e) {
+                            logger.info("Something went wrong in LsmDao ctor");
+                        } catch (NumberFormatException e) {
+                            logger.info("Unexpected name of SSTable file");
+                        }
+                    });
+            generation++;
+        }
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) throws IOException {
+        final List<Iterator<Cell>> iters = new ArrayList<>(ssTables.size() + 1);
+        iters.add(memtable.iterator(from));
+        ssTables.descendingMap().values().forEach(ssTable -> {
+            try {
+                iters.add(ssTable.iterator(from));
+            } catch (IOException e) {
+                logger.info("Something went wrong in iterator func");
+            }
+        });
+
+        final Iterator<Cell> mergedElements = Iterators.mergeSorted(iters, Cell.COMPARATOR);
+        final Iterator<Cell> freshElements = Iters.collapseEquals(mergedElements, Cell::getKey);
+        final Iterator<Cell> aliveElements = Iterators
+                .filter(freshElements, element -> !element.getValue().isTombstone());
+
+        return Iterators.transform(aliveElements, element -> Record.of(element.getKey(), element.getValue().getData()));
+    }
+
+    @Override
+    public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) throws IOException {
+        memtable.upsert(key, value);
+        if (memtable.getSizeInByte() > flushThreshold) {
+            flush();
+        }
+    }
+
+    @Override
+    public void remove(@NotNull final ByteBuffer key) throws IOException {
+        memtable.remove(key);
+        if (memtable.getSizeInByte() > flushThreshold) {
+            flush();
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (memtable.size() > 0) {
+            flush();
+        }
+        ssTables.values().forEach(Table::close);
+    }
+
+    private void flush() throws IOException {
+        final File file = new File(storage, generation + TEMP_FILE_POSTFIX);
+        file.createNewFile();
+        SSTable.serialize(file, memtable.iterator(EMPTY_BUFFER), memtable.size());
+        final File dst = new File(storage, generation + FILE_POSTFIX);
+        Files.move(file.toPath(), dst.toPath(), StandardCopyOption.ATOMIC_MOVE);
+        ++generation;
+        ssTables.put(generation, new SSTable(dst));
+        memtable.close();
+    }
 }
--- src/main/java/ru/mail/polis/valaubr/MemTable.java
+++ src/main/java/ru/mail/polis/valaubr/MemTable.java
@@ -1,4 +1,87 @@
 package ru.mail.polis.valaubr;
 
-public class MemTable {
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class MemTable implements Table {
+    /*
+     * ByteBuffer [link - 2 bytes
+     * + bytes in the buffer - n bytes
+     * + headline - 8 bytes
+     * + int inside 4 bytes
+     * + boolean inside - 1 bytes] Total: n + 15 bytes (about)
+     *
+     * Value [headline 8 bytes
+     * + ByteBuffer - n + 15 bytes
+     * + long - 8 bytes
+     * + link - 2 bytes] Total: n + 31 bytes (about)
+     *
+     * SortedMap [link - 2 bytes
+     * + headline - 8 bytes
+     * + for each object Key + Value
+     * + 4 bytes links in each object
+     * + 8 bytes on the headline of each node]
+     *
+     * Total: n + n + 68 (about when inserting a new object)
+     */
+    private final SortedMap<ByteBuffer, Value> map = new TreeMap<>();
+
+    private int sizeInBytes;
+
+    public MemTable() {
+        this.sizeInBytes = 0;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Cell> iterator(@NotNull final ByteBuffer from) {
+        return map.tailMap(from)
+                .entrySet()
+                .stream()
+                .map(entry -> new Cell(entry.getKey(), entry.getValue()))
+                .iterator();
+    }
+
+    @Override
+    public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) {
+        final Value val = map.get(key);
+        if (val == null) {
+            sizeInBytes += key.remaining() + value.remaining() + 68;
+
+        } else {
+            sizeInBytes += value.remaining() + 39;
+        }
+        map.put(key.duplicate(), new Value(System.currentTimeMillis(), value.duplicate()));
+    }
+
+    @Override
+    public void remove(@NotNull final ByteBuffer key) {
+        final Value val = map.get(key);
+        if (val == null) {
+            sizeInBytes += key.remaining() + Long.BYTES;
+        } else if (!val.isTombstone()) {
+            sizeInBytes -= val.getData().remaining();
+        }
+        map.put(key.duplicate(), new Value(System.currentTimeMillis()));
+    }
+
+    @Override
+    public long getSizeInByte() {
+        return sizeInBytes;
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public void close() {
+        map.clear();
+        sizeInBytes = 0;
+    }
 }
